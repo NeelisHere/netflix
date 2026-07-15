9f8b425f52facf4dba69e04413c445e79f17e2cb
Neelabh Paul <npaul_be19@thapar.edu>
2026-07-15 13:57:58 +0530

    first commit

---
+++ movie-service/src/main/java/com/netflix/movie_service/controller/MovieController.java
@@ -0,0 +1,12 @@
+package com.netflix.movie_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/movies")
+public class MovieController {
+    
+}
